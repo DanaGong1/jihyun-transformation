fb8b10340e3965d3c07cced5d50b7ce9edf8e55c
JihyunGong <jihyun2462@gmail.com>
2025-05-01 13:41:18 +0900

    chore: .gitignore, ESLint, Prettier, TypeScript 및 Vite 구성 파일 추가. 기본 HTML 및 CSS 파일 생성, React 및 Vite를 사용한 기본 애플리케이션 구조 설정

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import parser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+
+export default [
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+]

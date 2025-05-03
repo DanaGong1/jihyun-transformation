4eb624f84cf674c7573b7c731c7829ac6a539ea3
JihyunGong <jihyun2462@gmail.com>
2025-05-03 11:26:11 +0900

    feat: App 컴포넌트에 ControlsView 추가 - 사각형 조작 기능 통합

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 
 import { CanvasView } from './features/canvas'
+import { ControlsView } from './features/controls'
 import { useRectState } from './hooks'
 
 const AppContainer = styled.div`
@@ -9,16 +10,22 @@ const AppContainer = styled.div`
   padding: 20px;
   background-color: ${(props) => props.theme.colors.appBackground};
   min-height: 100vh;
-  align-items: center;
   justify-content: center;
 `
 
 function App() {
-  const { rect } = useRectState()
+  const { rect, cornerCoords, moveRect, rotateRect, changePivot } = useRectState()
 
   return (
     <AppContainer>
       <CanvasView rect={rect} />
+      <ControlsView
+        rect={rect}
+        cornerCoords={cornerCoords}
+        onMove={moveRect}
+        onRotate={rotateRect}
+        onPivotChange={changePivot}
+      />
     </AppContainer>
   )
 }
